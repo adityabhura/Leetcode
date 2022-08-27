80ceed383da5092f4f9f2ddbdfbf41a53d1c80b0
adityabhura <62247947+adityabhura@users.noreply.github.com>
2022-08-27 23:01:35 +0530

    Time: 368 ms (86.39%), Space: 45.7 MB (77.62%) - LeetHub

---
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    int reversePairs(vector<int>& arr) {
+        int n=arr.size();
+        help(arr,0,n-1);
+        return ans;
+    }
+    
+    
+    long long ans=0;
+
+void merge(vector<int>& arr, int l,int r,int mid){
+    long long int temp[r-l+1];
+    long long int i=l;
+    long long int j=mid+1;
+    for(i;i<=mid;i++){
+while( j<=r && arr[i]>1ll*2*arr[j] ){
+                j++;
+        }
+        ans+=(j-(mid+1));
+    }
+    
+    
+    
+    i=l;
+    j=mid+1;
+    long long int k=0;
+    while(i<=mid && j<=r){
+        if(arr[i]<arr[j])temp[k++]=arr[i++];
+        else temp[k++]=arr[j++];
+    }
+    while(i<=mid){
+        temp[k++]=arr[i++];
+        
+    }
+    while(j<=r){
+        temp[k++]=arr[j++];
+    }
+    
+    for(int m=0,n=l;m<(r-l+1);m++,n++){
+        arr[n]=temp[m];
+    }
+    
+}
+
+void help(vector<int>& arr, int l,int r){
+    if(l>=r)return;
+    int mid=(l+(r-l)/2);
+    help(arr,l,mid);
+    help(arr,mid+1,r);
+    merge(arr,l,r,mid);
+}
+
+
+    
+    
+};
