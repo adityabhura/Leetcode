230108f8eeffb78b8470c02e8831e9e0d65f0e78
adityabhura <62247947+adityabhura@users.noreply.github.com>
2022-09-04 02:13:18 +0530

    Time: 11 ms (51.07%), Space: 12.7 MB (78.31%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -12,22 +12,37 @@
 class Solution {
 public:
     
-    void preorder(vector<int> &v,TreeNode* root){
-        if(root==NULL)return;
-        v.push_back(root->val);
-        preorder(v,root->left);
-        preorder(v,root->right);
-    }
+    // void preorder(vector<int> &v,TreeNode* root){
+    //     if(root==NULL)return;
+    //     v.push_back(root->val);
+    //     preorder(v,root->left);
+    //     preorder(v,root->right);
+    // }
     
     void flatten(TreeNode* root) {
-        vector<int> v;
-        preorder(v,root);
-        if(root==NULL)return ;
-        root->left=NULL;
-        root->right=NULL;
-        for(int i=1;i<v.size();i++){
-            root->right=new TreeNode(v[i]);
-            root=root->right;
+        // vector<int> v;
+        // preorder(v,root);
+        // if(root==NULL)return ;
+        // root->left=NULL;
+        // root->right=NULL;
+        // for(int i=1;i<v.size();i++){
+        //     root->right=new TreeNode(v[i]);
+        //     root=root->right;
+        // }
+        
+        TreeNode* curr=root;
+        while(curr){
+            if(curr->left){
+                TreeNode* prev=curr->left;
+                while(prev->right){
+                    prev= prev->right;
+                }
+                prev->right=curr->right;
+                curr->right=curr->left;
+                curr->left=NULL;
+            }
+            curr=curr->right;
         }
+        
     }
 };
